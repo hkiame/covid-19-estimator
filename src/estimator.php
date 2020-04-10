def10cae5b67b688107f6b378bf7020e92464c9c
= <=>
2020-04-11 00:23:59 +0300

    debugging

--- src/estimator.php
+++ src/estimator.php
@@ -1,18 +1,18 @@
 <?php
 
-$data = [
-  "region" => [
-    "name" => "Africa",
-    "avgAge" => 19.7,
-    "avgDailyIncomeInUSD" => 4,
-    "avgDailyIncomePopulation" => 0.73
-  ],
-  "periodType" => "days",
-  "timeToElapse" => 38,
-  "reportedCases" => 2747,
-  "population" => 92931687,
-  "totalHospitalBeds" => 678874
-];
+// $data = [
+//   "region" => [
+//     "name" => "Africa",
+//     "avgAge" => 19.7,
+//     "avgDailyIncomeInUSD" => 4,
+//     "avgDailyIncomePopulation" => 0.73
+//   ],
+//   "periodType" => "days",
+//   "timeToElapse" => 38,
+//   "reportedCases" => 2747,
+//   "population" => 92931687,
+//   "totalHospitalBeds" => 678874
+// ];
 
 
 
@@ -107,6 +107,6 @@ function incomeLost($infected, $avgDailyIncomePop, $avgDailyIncome, $days){
   return number_format($result, 2, '.', '');
 }
 
-echo "<pre>";
-print_r(covid19ImpactEstimator($data));
-echo "</pre>";
+// echo "<pre>";
+// print_r(covid19ImpactEstimator($data));
+// echo "</pre>";
